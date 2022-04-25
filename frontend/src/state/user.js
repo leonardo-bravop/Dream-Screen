fd094eefbed4cd52877a8aea42c1ba2e6dafc93d
Leonardo Bravo P <leo.bravoparedes@gmail.com>
2022-04-25 09:21:19 -0300

    configured redux

---
+++ frontend/src/state/user.js
@@ -0,0 +1,58 @@
+import { createReducer, createAsyncThunk } from "@reduxjs/toolkit";
+import axios from "axios";
+
+export const sendLoginRequest = createAsyncThunk("LOGIN", (userData) => {
+  return axios.post("/api/login", userData).then((res) => res.data);
+});
+
+export const sendLogoutRequest = createAsyncThunk("LOGOUT", () => {
+  return axios.post("/api/logout").then((res) => res.data);
+});
+
+export const persistUser = createAsyncThunk("PERSIST", () => {
+  return axios.get("/api/me").then((res) => res.data);
+});
+
+export const sendSignUpRequest = createAsyncThunk("REGISTER", (userData) => {
+  return axios.post("/api/register", userData).then((res) => res);
+});
+
+export const addToFavoriteMovies = createAsyncThunk(
+  "ADD_FAVORITE_MOVIES",
+  (object, thunkAPI) => {
+    const { user } = thunkAPI.getState();
+    return axios
+      .put(
+        `/api/favorites?userId=${user.id}&mediaType=${object.mediaType}&mediaId=${object.mediaId}`
+      )
+      .then((res) => res.data);
+  }
+);
+
+export const RemoveFromFavorites = createAsyncThunk(
+  "REMOVE_FROM_FAVORITE",
+  (object, thunkAPI) => {
+    const { user } = thunkAPI.getState();
+    return axios
+      .delete(
+        `/api/favorites?userId=${user.id}&mediaType=${object.mediaType}&MediaId=${object.mediaId}`
+      )
+      .then((res) => res.data);
+  }
+);
+
+const userReducer = createReducer(
+  {},
+  {
+    [sendLoginRequest.fulfilled]: (state, action) => action.payload,
+    [sendSignUpRequest.fulfilled]: (state, action) => action.payload,
+    [sendLogoutRequest.fulfilled]: (state, action) => {
+      return {};
+    },
+    [persistUser.fulfilled]: (state, action) => action.payload,
+    [addToFavoriteMovies.fulfilled]: (state, action) => action.payload[1][0],
+    [RemoveFromFavorites.fulfilled]: (state, action) => action.payload[1][0],
+  }
+);
+
+export default userReducer;
