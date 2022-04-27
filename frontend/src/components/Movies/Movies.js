776c09f0fbe01f173da6f5a5c0e8f33285996bfa
Leonardo Bravo P <leo.bravoparedes@gmail.com>
2022-04-27 00:43:44 -0300

    updated tv shows screen

--- frontend/src/components/Movies/Movies.js
+++ frontend/src/components/Movies/Movies.js
@@ -5,61 +5,28 @@ import "./Movies.css";
 
 const Movies = () => {
   const [selected, setSelected] = useState("popular");
+  const statesArray = ["popular", "now_playing", "upcoming", "top_rated"];
   return (
     <>
       <div className="movies-tagnav">
-        <Link to="popular">
-          <div className="tagnav-div">
-            <button
-              className="tagnav-button"
-              onClick={() => setSelected("popular")}
-            >
-              Popular
-            </button>
-            {selected === "popular" && (
-              <img src="/line.svg" className="tagnav-line" />
-            )}
-          </div>
-        </Link>
-        <Link to="now-playing">
-          <div className="tagnav-div">
-            <button
-              className="tagnav-button"
-              onClick={() => setSelected("now_playing")}
-            >
-              Now playing
-            </button>
-            {selected === "now_playing" && (
-              <img src="/line.svg" className="tagnav-line" />
-            )}{" "}
-          </div>
-        </Link>
-        <Link to="upcoming">
-          <div className="tagnav-div">
-            <button
-              className="tagnav-button"
-              onClick={() => setSelected("upcoming")}
-            >
-              Upcoming
-            </button>
-            {selected === "upcoming" && (
-              <img src="/line.svg" className="tagnav-line" />
-            )}{" "}
-          </div>
-        </Link>
-        <Link to="top-rated">
-          <div className="tagnav-div">
-            <button
-              className="tagnav-button"
-              onClick={() => setSelected("top_rated")}
-            >
-              Top rated
-            </button>
-            {selected === "top_rated" && (
-              <img src="/line.svg" className="tagnav-line" />
-            )}{" "}
-          </div>
-        </Link>
+        {statesArray.map((state) => {
+          return (
+            <Link to={state}>
+              <div className="tagnav-div">
+                <button
+                  className="tagnav-button"
+                  onClick={() => setSelected(state)}
+                >
+                  {state[0].toUpperCase() +
+                    state.slice(1, state.length).split("_").join(" ")}
+                </button>
+                {selected === state && (
+                  <img src="/line.svg" className="tagnav-line" />
+                )}
+              </div>
+            </Link>
+          );
+        })}
       </div>
       <MediaRow mediaType={"movie"} state={selected} />
     </>
--- frontend/src/components/TvShows/TvShows.js
+++ frontend/src/components/TvShows/TvShows.js
@@ -1,13 +1,32 @@
 import { useState } from "react";
+import { Link } from "react-router-dom";
 import MediaRow from "../MediaRow";
 
 const TvShows = () =>{
   const [selected, setSelected] = useState("popular");
+  const statesArray = ["popular", "top_rated"];
 
     return (
        <>
-        <div>
-        <h2>Popular Tv Shows:</h2>
+        <div className="movies-tagnav">
+        {statesArray.map((state) => {
+          return (
+            <Link to={state}>
+              <div className="tagnav-div">
+                <button
+                  className="tagnav-button"
+                  onClick={() => setSelected(state)}
+                >
+                  {state[0].toUpperCase() +
+                    state.slice(1, state.length).split("_").join(" ")}
+                </button>
+                {selected === state && (
+                  <img src="/line.svg" className="tagnav-line" />
+                )}
+              </div>
+            </Link>
+          );
+        })}
       </div>
         <MediaRow mediaType={"tv"} state={selected}/></>
     )
